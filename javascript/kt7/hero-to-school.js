3b19b42bc08faea56579c8712ed66f377f7a37ad
smoearn <sverre_91@hotmail.com>
2019-01-03 16:37:21 +0100

    adding javascript kata6

--- javascript/kt7/hero-to-school.js
+++ javascript/kt7/hero-to-school.js
@@ -1,22 +1,18 @@
 function whichBusToTake(busesColors, goingToSchool) {
-    let red = 0
-    let blue = 0
+
     for (let i = 0; i < busesColors.length; i++) {
         if (busesColors[i] == 'red' && goingToSchool[i] == true){
-            red = i
-            break
+            return i
         }
     }
 
     for (let i = 0; i < busesColors.length; i++) {
         if (busesColors[i] == 'blue' && goingToSchool[i] == true){
-            blue = i
-            break
+            return i
+            
         }
     }
-    console.log(red);
-    
-    return red < blue ? red:blue
+
     
   }
 
@@ -28,14 +24,14 @@ function whichBusToTake(busesColors, goingToSchool) {
     ["red","red","blue"],
     [true, true, true]),0)
     
-    // console.log(whichBusToTake(
-    // ["blue","blue","blue","red","red"],
-    // [false, true, true, true, false]),3)
+    console.log(whichBusToTake(
+    ["blue","blue","blue","red","red"],
+    [false, true, true, true, false]),3)
     
-    // console.log(whichBusToTake(
-    // ["blue","red","red","red","blue","red","blue"],
-    // [true, false, false, false, true, true, false]),5)
+    console.log(whichBusToTake(
+    ["blue","red","red","red","blue","red","blue"],
+    [true, false, false, false, true, true, false]),5)
     
-    // console.log(whichBusToTake(
-    // ["red","red","red","blue"],
-    // [false, false, false, true]),3)
+    console.log(whichBusToTake(
+    ["red","red","red","blue"],
+    [false, false, false, true]),3)
